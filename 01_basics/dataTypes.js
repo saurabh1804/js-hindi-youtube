a85939cfd8e85e5a23615dcc9a7c64194ad1248c
saurabh1804 <saurabhkumar1842000@gmail.com>
2026-01-13 16:53:18 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,24 @@
+"used strict"; //treat all js code as newer version
+//alert(3+3) //we are using node js ,not browser
+console.log(3
+    +
+    3)  // code readablity should be high
+console.log("saurabh singh")
+let name="hitesh"  //string
+let age=18 //number or int
+let LoggedIn=false//boolean 
+/*
+premitive data types
+*/
+//number=2 to power 53 //it is range
+//bigint=//bade no ke liye bigint ka use karte hai
+//string =" "
+//boolean=true/false
+//null=standalone value
+//undefined=only for decreasing variable but not assign
+//symbol=uniqueness
+//object
+
+
+console.log(typesOf,age);
+
