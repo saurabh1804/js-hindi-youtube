6c8fc76e8d0ada184795a95e03cffc298c110480
saurabh1804 <saurabhkumar1842000@gmail.com>
2026-01-20 18:58:02 +0000

    conversionOperation of dataTypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,28 @@
+let score=true// "33 abc"
+// console.log(typeof score);
+// let scores=undefined
+// console.log(typeof scores);
+// console.log(score)
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+//"33 abc" => NaN
+// true => 1
+
+let isLoggedIn="saurabh"     //""      //0  //1
+let BooleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(BooleanIsLoggedIn);
+
+// 1=true;
+// 0=false;
+// ""=false
+// "saurabh"=true
+
+let someNumber=33
+let StringNumber=String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
