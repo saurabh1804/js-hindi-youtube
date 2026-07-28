5804f175ba666cc2567cf043c90b375b3dd4d68b
saurabh1804 <saurabhkumar1842000@gmail.com>
2026-07-28 19:47:10 +0000

    contains all operations

---
+++ .devcontainer/02_basics/conversionOperation.js
@@ -0,0 +1,45 @@
+/*let score="33abc"
+console.log(score)
+console.log(typeof(score))
+let valueInNumber=Number(score)
+console.log(valueInNumber)
+//"33"=33
+//"33abc"=NaN
+//true=1
+//false=0
+let IsLoggedIn=1 //empty=false //saurabh=true
+let BooleanIsLoggedIn=Boolean(IsLoggedIn)
+console.log(BooleanIsLoggedIn);
+let someNumber=33
+let StringNumber=String(someNumber)
+console.log(someNumber)
+console.log(StringNumber)
+console.log(typeof(StringNumber))*/
+
+//operations
+/*let value=3
+let negValue=-value
+console.log(negValue)
+console.log(2+2)
+console.log(4-2)
+console.log(4*2)
+console.log(4**4)
+let str1="hello"
+let str2="saurabh"
+let str3=str1+ str2
+console.log(str3)*/
+//console.log("1"+2);
+//console.log(1+"2")
+//console.log("1"+"2")
+// console.log("1"+2+2);
+// console.log(1+2+"2")
+// console.log((3+4)*5%3)
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
+let gameCounter=100
+++gameCounter
+console.log(gameCounter)
