50a7c1aed3cdbfeebec313527933d3b89344636e
saurabh1804 <saurabhkumar1842000@gmail.com>
2026-07-28 18:24:23 +0000

    contain to data

---
+++ .devcontainer/02_basics/dataTypes1.js
@@ -0,0 +1,17 @@
+"used strict"; //treat all js code as newer version
+//alert(3+3) 
+console.log(3+3)
+console.log("saurabh") //code readability should be high
+let Name="saurabh"  // string
+console.log(typeof ("saurabh")) //String type
+let age=18 // number or int
+let IsLoggedIn=false //boolean
+//number=2 to power 53 //it is range
+//bigInt=bade no ke liye bigInt ka use karte hai
+//string=""
+//Bollean=true/false
+//Null=standlane value
+//undefined=only for declaring variable but not assign
+//symbol=uniqueness
+console.log(typeof(null))
+console.log(undefined)
---
+++ .devcontainer/02_basics/variable1.js
@@ -0,0 +1,10 @@
+const accountId=144553
+let accountEmail="saurabh221@gmail.com"
+var accountPassword="1234"
+accountCity="jaypur"
+console.log(accountId);
+let accountState;
+accountEmail="saurabh@123"
+accountPassword="21212121"
+accountCity="Delhi"
+console.table([accountId , accountEmail , accountPassword , accountCity])
