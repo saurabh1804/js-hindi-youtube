c520a465fdfdd238ddd747821c85bf68c5273263
saurabh1804 <saurabhkumar1842000@gmail.com>
2026-07-29 20:28:45 +0000

    explain data types

---
+++ .devcontainer/02_basics/comparison.js
@@ -0,0 +1,15 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log("2">1)// in dono string ko number main convert kar de raha hai
+// console.log("02">1)
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)//jab comparison check karte hai to null ko zero main convert kar dete hai
+// console.log(undefined>0)
+// console.log(undefined==0)
+// console.log(undefine<0)// java script ke andar comparison aur equality check dono alag hote hai
+//STRICT CHECK
+console.log("2"===2)//strict check main only comparison nahi hota data types bhi check hote hai
---
+++ .devcontainer/02_basics/dataTypes.js
@@ -0,0 +1,40 @@
+// const score=100
+// const scorevalue=100.3
+// const isLoggedIn=false
+// const outSideTemp=null
+// let userEmail;
+// const id=symbol('123')
+// // const anotherId=symbol('123')
+// // const bigInt=1234123412341234n
+// console.log(score)
+// console.log(scorevalue)
+//+++++++++++++++++++++++++++++++
+
+// const heros=["shaktiman" , "naagraj" , "doga"]
+// console.log(heros)
+// let myObj={
+//     name:"saurabh kumar",
+//     age:22,
+// }
+// console.log(myObj)
+
+// const myFunction=function(){
+//     console.log("hello world")
+// }
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// let myYoutubename="saurabhsingh.com"
+// let anothername="myYoutubename"
+// anothername="chaiaurcode"
+// console.log(myYoutubename)
+// console.log(anothername)
+
+let userOne={
+    email:"saurabh@123",
+    upi:"saurabh@ybl"
+}
+let userTwo=userOne
+userTwo.email="shakti@123"
+console.log(userOne)
+console.log(userTwo)
