0bd34a27d6a4e0ad49262b2a9215292915556ea1
saurabh1804 <saurabhkumar1842000@gmail.com>
2026-07-31 04:08:20 +0000

    timeDate explaination and deep learning

---
+++ .devcontainer/02_basics/dateTime.js
@@ -0,0 +1,27 @@
+// let myDate=new Date()
+// console.log(myDate) //ise completly date samjh nahi aata
+// console.log(myDate.toString()) //but is toString() function ka use karne se completely date samjh main aata hai
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toTimeString()) //time screen main us date ka time batata hai
+// console.log(typeof(myDate)) //my Date ka type object hi hota hai
+// let myCreatedDate=new Date(2023,0,23)
+// console.log(myCreatedDate.toDateString()) //ye specific date batata hai jo aapne input main diya hai
+// let myCreatedDate=new Date(2023,0,23,5,3)
+// console.log(myCreatedDate.toLocaleDateString())
+// let myCreatedDate=new Date("2023-01-14")   //yaha month 01 se start hota hai
+// console.log(myCreatedDate.toLocaleDateString())
+// let myCreatedDate=new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleDateString())
+
+//+++++++++++++ TIME +++++++++++++++++
+// let myTimeStamp=Date.now()
+// console.log(myTimeStamp) //jab quizes design hota hai ya pole design hota haiwha time stamp ka use hota h
+// console.log(myreatedDate.getTime()) //agar kisi date se mujhe eak specific time nikalana haieasily compare kar sakte hai
+// console.log(Math.floor(Date.now())/1000) //ise second main compare karne ke liye 1000 se devide karte haiand decimal hatane ke liye floor ka use karte hai
+// console.log(Date.now()/1000)
+
+let myNewDate=myNewDate()
+console.log(myNewDate)
