c7643f0c5ab4f35d383dcd8d8a178ca03ddd54b3
saurabh1804 <saurabhkumar1842000@gmail.com>
2026-08-18 19:58:22 +0000

    explaination control flow and details get information more next leacture

---
+++ .devcontainer/02_basics/controlFlow.js
@@ -0,0 +1,69 @@
+//control flow main eak time par sabhi code run nahi karte hai agar login huaa to eak time par eak code run huaa logout huaa to koi dusra part run hoga
+//if
+// if(true){
+
+// }
+// if(false){
+
+// }
+// if(2==2){ 
+//     console.log(true);
+
+// }
+// const isUserloggedIn=true
+// if(isUserloggedIn){
+
+// }
+
+// if(2===2){
+//     console.log("executed");
+// }
+// if(2==="2"){
+//     console.log("executed");
+// }
+
+// const temperature=41
+// if(temperature<50){
+//     console.log("less than 50");
+// }
+// console.log("temperature is greater than 50");
+
+
+// const temperature=40
+// if(temperature===39){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("temperature is greater than 50")
+// }
+
+// const score=200
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power:${power}`);
+// }
+
+
+ const balance=1000
+// if(balance>500) console.log("test1"), console.log("test2");
+// eak baat hamesa yaad rakhna mera commas lagakar kaam karne se syntax sahi nahi hota hai isliye hum commas dekar nahi likhte hai
+
+// if(balance<500){
+//     console.log("less than 500");
+// }
+// else if(balance<750){
+//     console.log("less than 750");
+// }
+// else if(balance<900){
+//     console.log("less than 900")
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+const loggedInFormEmail=true
+const loggedInFormGoogle=false
+const debitCard=true
+if(UserLoggedIn && debitCard){
+    console.log("Allow to buy course");
+}
