dc0d791ac497cd75a5c61a9176e7bda299bf70e2
saurabh1804 <saurabhkumar1842000@gmail.com>
2026-08-23 20:11:05 +0000

    control flow statement all explaination

--- .devcontainer/02_basics/controlFlow.js
+++ .devcontainer/02_basics/controlFlow.js
@@ -10,7 +10,7 @@
 //     console.log(true);
 
 // }
-// const isUserloggedIn=true
+//const isUserloggedIn=true
 // if(isUserloggedIn){
 
 // }
@@ -61,9 +61,119 @@
 //     console.log("less than 1200");
 // }
 
-const loggedInFormEmail=true
-const loggedInFormGoogle=false
-const debitCard=true
-if(UserLoggedIn && debitCard){
-    console.log("Allow to buy course");
+// const loggedInFormEmail=true
+// const loggedInFormGoogle=false
+// const UserLoggedIn=true
+// const debitCard=true
+// if(UserLoggedIn && debitCard){
+//     console.log("Allow to buy course");
+// }
+// if(loggedInFormGoogle || loggedInFormEmail){
+//     console.log("userLoggedIn")
+// }
+
+
+// //Switch
+// const month=3;
+// switch(key){
+//     case Value:
+//         break;
+//         default:
+//             break;
+// }
+//key wo value hota hai jise hume check karna hota hai
+
+// const month=3
+// switch(month){
+//     case 1:
+//         console.log("january");
+//         break;
+//         case 2:
+//             console.log("February");
+//             break;
+//             case 3:
+//                 console.log("March");
+//                 break
+//                 case 4:
+//                     console.log("April");
+//                     break;
+//                     default:
+//                         console.log("default case match");
+//                         break;
+// }
+
+const month="march"
+switch(month){
+    case 1:
+        console.log("januray");
+        break;
+        case 2:
+            console.log("February");
+            break;
+            case 3:
+                console.log("march");
+                break;
+                case 4:
+                    console.log("April");
+                    break;
+                    default:
+                        console.log("default case match");
+                        break;
 }
+
+//trithy.js
+
+// const userEmail=[]
+// // const userEmail="@saurabh123"
+// // const userEmail=""
+// if(userEmail){
+//     console.log("got user email");
+// }
+// else{
+//     console.log("dont have user email");
+// }
+// const userEmail="@saurabh123"
+// // const userEmail=""
+// if(userEmail){
+//     console.log("got user email");
+// // }
+// // else{
+// //     console.log("dont have user email");
+// // }
+// const userEmail=""
+// // if(userEmail){
+// //     console.log("got user email");
+// // }
+// // else{
+// //     console.log("dont have user email");
+// // }
+
+// // Falsy Value
+// // False , 0 , -0 , BigInt on , "" , null , undefined , NaN 
+
+// //Truthy
+// //"0" , 'false' , " " , [] , { } , function() {} - empty function
+
+// if(userEmail.length===0){
+//     console.log("Array is empty");
+
+//     const emptyObj={}
+//     if(Object.keys(emptyObj).length===0){
+//         console.log("obj is empty");
+//     }
+// }
+
+
+//nullish coalesciung operators(??):null undefined
+
+// let val1;
+// val1=5??10
+// val1=null??10
+// val1=undefined??5
+// console.log(val1);
+
+//terniary operator
+
+condition?true:false
+const iceTeaPrice=100
+iceTeaPrice>=80?console.log("less than 80"):console.log("more than 80");
