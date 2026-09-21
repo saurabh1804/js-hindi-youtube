205df4a564b994522b46d13f909ff7ffecf13abb
saurabh1804 <saurabhkumar1842000@gmail.com>
2026-09-21 19:05:01 +0000

    loop explaination and practice code

---
+++ .devcontainer/02_basics/loop.js
@@ -0,0 +1,21 @@
+// let index=1
+// while(index<=10){
+//     console.log(`index of value is ${index}`);
+//     index++;
+// }
+
+let myArray=["flash" , "batman" , "superman"];
+let arr=0
+while(arr<myArray.length){
+    //console.log(`my array is ${myArray}`);
+    arr++
+}
+
+// USING DO WHILE LOOP
+
+let score=1
+do{
+    console.log(`score is score ${score}`);
+    score++
+}
+while(score<=10);
