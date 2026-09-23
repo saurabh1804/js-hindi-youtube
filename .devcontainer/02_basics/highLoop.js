da5b34c7e8cde3395470d497fafe8c1116d023d6
saurabh1804 <saurabhkumar1842000@gmail.com>
2026-09-23 19:37:49 +0000

    highLoop some more explaination

--- .devcontainer/02_basics/highLoop.js
+++ .devcontainer/02_basics/highLoop.js
@@ -34,70 +34,137 @@
 // }
 // for (const [key , value] of myObject) {
 //     console.log(key , ':' , value);
+// // }
+// // //Here show the error it is not iterable that means my object is not iterable
+
+//     const myObject={
+//     js: 'javaScript' , 
+//     cpp: 'c++' ,
+//     rb: "ruby" , 
+//     swift : "swift by apple"
 // }
-// //Here show the error it is not iterable that means my object is not iterable
-
-    const myObject={
-    js: 'javaScript' , 
-    cpp: 'c++' ,
-    rb: "ruby" , 
-    swift : "swift by apple"
-}
-for (const key in myObject) {
+// for (const key in myObject) {
    
-    console.log(`${key} shortcut is for ${myObject[key]}`);
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
     
-}
+// }
 
-//object ke liye for-of loop kaam nahi karta
-//object ke liye for-in ka kaam hota hai but aisa nahi hai ki only object ke liye hota hai ye kuchh array ke liye bhi kaam karta hai
+// //object ke liye for-of loop kaam nahi karta
+// //object ke liye for-in ka kaam hota hai but aisa nahi hai ki only object ke liye hota hai ye kuchh array ke liye bhi kaam karta hai
 
-const programming=["js" , "rb" , "py" , "java"]
-console.log(programming[key]);
-//for-in ko jab hum array par apply karte hai usme key print karte hai to array ki indexing aati aur yahi object ko print karte hai to value aati hai
+// const programming=["js" , "rb" , "py" , "java"]
+// console.log(programming[key]);
+// //for-in ko jab hum array par apply karte hai usme key print karte hai to array ki indexing aati aur yahi object ko print karte hai to value aati hai
 
-//thik waise hi for-of array par apply krte hai to sidha value aati hai
+// //thik waise hi for-of array par apply krte hai to sidha value aati hai
 
-const map=new Map()
-map.set('In' , "India");
-map.set('USA' , "America");
-map.set('fr' , "france");
-for (const key in Map) {
-  console.log(key)
+// const map=new Map()
+// map.set('In' , "India");
+// map.set('USA' , "America");
+// map.set('fr' , "france");
+// for (const key in Map) {
+//   console.log(key)
     
     
-}
-//for-in map ke iteratible nahi hoga
+// }
+// //for-in map ke iteratible nahi hoga
 
 
-//# For each
-const coding=["js" , "ruby" , "java" , "python" , "cpp"]
-//coding.forEach(function name(){}) aise hi hum function declare karte hai but ye call back use karta hai so iska name nahi hoga
+// //# For each
+ const coding=["js" , "ruby" , "java" , "python" , "cpp"]
+// //coding.forEach(function name(){}) aise hi hum function declare karte hai but ye call back use karta hai so iska name nahi hoga
 
-coding.forEach(function(val){
-    console.log(val);
+// coding.forEach(function(val){
+//     console.log(val);
+// })
+
+// function PrintMe(item){
+//     console.log(item);
+// }
+// coding.forEach(PrintMe)
+
+// coding.forEach((item , index , arr)=>{
+//     console.log(item , index , arr);
+// })
+
+// const myCoding=[{
+//     languageName: "javaScript",
+//     languageFileName: "js"
+// }]
+
+// {
+//     languageName: "java"
+//     languageFileName:"py"
+// }
+
+// coding.forEach((item)=>{
+//     console.log(item.languageName);
+//     console.log(item.languageFileName)
+// })
+
+//for each:
+const value =coding.forEach((item)=>{
+   console.log(item);
+   return item
 })
+console.log(value)
 
-function PrintMe(item){
-    console.log(item);
-}
-coding.forEach(PrintMe)
+//for each value return nahin karta hai
 
-coding.forEach((item , index , arr)=>{
-    console.log(item , index , arr);
+//**************************************************************************** */
+//using Map & Filter in javaScript
+// const myNums=[1,2,3,4,5,6,7,8,9,10]
+// // myNum.filter((num)=>num>4);
+// const newNums=myNums.filter((nums)=>nums>4)
+// console.log(newNums);
+
+//Using for-Each
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+const newNums=[]
+myNums.forEach((nums)=>{
+    if(nums>4){
+        newNums.push(nums)
+    }
 })
+console.log(newNums)
 
-const myCoding=[{
-    languageName: "javaScript",
-    languageFileName: "js"
-}]
 
-{
-    languageName: "java"
-    languageFileName:"py"
-}
 
-coding.forEach((item)=>{
-    console.log(item.languageName);
-    console.log(item.languageFileName)
+
+
+
+
+
+const books=[
+    {
+        title:'Book one' , genere:'Fiction' , publish:'1981' , edition:'2004'
+    },
+     {
+        title:'Book two' , genere:'non-Fiction' , publish:'1992' , edition:'2008'
+    },
+     {
+        title:'Book three' , genere:'History' , publish:'1982' , edition:'2006'
+    },
+     {
+        title:'Book Four' , genere:'Non-Fiction' , publish:'1988' , edition:'2004'
+    },
+     {
+        title:'Book five' , genere:'Science' , publish:'2000' , edition:'2007'
+    },
+     {
+        title:'Book six' , genere:'Fiction' , publish:'2001' , edition:'2014'
+    },
+];
+
+let userBooks=books.filter((bk)=>bk.genere==='history')
+console.log(userBooks)
+
+userBooks=books.filter((bk)=>{
+    return bk.publish>=2000
 })
+console.log(userBooks);
+
+userBooks=books.filter((bk)=>{
+    return bk.publish>=2000 && bk.genere==='history'
+})
+console.log(userBooks);
